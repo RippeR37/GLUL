962cc88fb05e247cf2f26c4a61a03f05cb8f579b
RippeR <ripper37@gmail.com>
2015-01-23 23:46:30 +0100

    Simple String class containing static methods to operate on string. For now its only split functionality.

---
+++ include/Utils/String.h
@@ -0,0 +1,27 @@
+#ifndef UTILS_STRING_H_INCLUDED
+#define UTILS_STRING_H_INCLUDED
+
+#include <string>
+#include <vector>
+
+namespace Util {
+
+    class String {
+        public:
+            static std::vector<std::string>  split(
+                std::vector<std::string> &input, 
+                const std::string& data, 
+                char delimiter, 
+                bool skipEmpty = true
+            );
+
+            static std::vector<std::string>& split(
+                const std::string& data, 
+                char delimiter, 
+                bool skipEmpty = true
+            );
+    };
+
+}
+
+#endif
---
+++ src/Utils/String.cpp
@@ -0,0 +1,26 @@
+#include <Utils/String.h>
+
+#include <sstream>
+#include <istream>
+
+namespace Util {
+
+    std::vector<std::string> String::split(std::vector<std::string> &input,  const std::string& data, char delimiter,  bool skipEmpty) {
+        std::stringstream stream(data);
+        std::string token;
+
+        while(std::getline(stream, token, delimiter)) {
+            if(!token.empty() || skipEmpty)
+                input.push_back(token);
+        }
+
+        return input;
+    }
+
+    static std::vector<std::string>& split(const std::string& data, char delimiter, bool skipEmpty) {
+        std::vector<std::string> tokens;
+        String::split(tokens, data, delimiter, skipEmpty);
+        return tokens;
+    }
+
+}
