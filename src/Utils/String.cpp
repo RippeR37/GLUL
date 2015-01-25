RippeR37/GLUL

commit 962cc88fb05e247cf2f26c4a61a03f05cb8f579b

Simple String class containing static methods to operate on string. For now its only split functionality.

project/Utility Library.vcxproj
@@ -109,6 +109,7 @@
     <ClInclude Include="..\include\Utils\Line.h" />
     <ClInclude Include="..\include\Utils\Point.h" />
     <ClInclude Include="..\include\Utils\Rectangle.h" />
+    <ClInclude Include="..\include\Utils\String.h" />
     <ClInclude Include="..\include\Utils\TimeEvent.h" />
     <ClInclude Include="..\include\Utils\TimeLoop.h" />
     <ClInclude Include="..\include\Utils\Timer.h" />
@@ -140,6 +141,7 @@
     <ClCompile Include="..\src\Utils\Line.cpp" />
     <ClCompile Include="..\src\Utils\Point.cpp" />
     <ClCompile Include="..\src\Utils\Rectangle.cpp" />
+    <ClCompile Include="..\src\Utils\String.cpp" />
     <ClCompile Include="..\src\Utils\TimeEvent.cpp" />
     <ClCompile Include="..\src\Utils\TimeLoop.cpp" />
     <ClCompile Include="..\src\Utils\Timer.cpp" />

src/Utils/String.cpp
@@ -1,0 +1,26 @@
+#include <Utils/String.h>
+
+#include <sstream>
+#include <istream>
+
+namespace Util {
+
+    std::vector<std::string> String::split(std::vector<std::string> &input,  const std::string& data, char delimiter,  bool skipEmpty) {
+        std::stringstream stream(data);
+        std::string token;
+
+        while(std::getline(stream, token, delimiter)) {
+            if(!token.empty() || skipEmpty)
+                input.push_back(token);
+        }
+
+        return input;
+    }
+
+    static std::vector<std::string>& split(const std::string& data, char delimiter, bool skipEmpty) {
+        std::vector<std::string> tokens;
+        String::split(tokens, data, delimiter, skipEmpty);
+        return tokens;
+    }
+
+}

include/Utils/String.h
@@ -1,0 +1,27 @@
+#ifndef UTILS_STRING_H_INCLUDED
+#define UTILS_STRING_H_INCLUDED
+
+#include <string>
+#include <vector>
+
+namespace Util {
+
+    class String {
+        public:
+            static std::vector<std::string>  split(
+                std::vector<std::string> &input, 
+                const std::string& data, 
+                char delimiter, 
+                bool skipEmpty = true
+            );
+
+            static std::vector<std::string>& split(
+                const std::string& data, 
+                char delimiter, 
+                bool skipEmpty = true
+            );
+    };
+
+}
+
+#endif

project/Utility Library.vcxproj.filters
@@ -134,6 +134,9 @@
     <ClInclude Include="..\include\Utils\Interfaces\MVC\View.h">
       <Filter>Header Files\Interfaces\MVC</Filter>
     </ClInclude>
+    <ClInclude Include="..\include\Utils\String.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
   </ItemGroup>
   <ItemGroup>
     <ClCompile Include="..\src\Utils\Clock.cpp">
@@ -222,6 +225,9 @@
     </ClCompile>
     <ClCompile Include="..\src\Utils\Interfaces\Camera.cpp">
       <Filter>Source Files\Interfaces</Filter>
+    </ClCompile>
+    <ClCompile Include="..\src\Utils\String.cpp">
+      <Filter>Source Files</Filter>
     </ClCompile>
   </ItemGroup>
 </Project>

commit 35d6c9a0df1833ec379b0b3862a95d59ccaed070

Fixed issue with string splitting introduced in 962cc88.

project/Utility Library.vcxproj
@@ -53,8 +53,9 @@
     <ClCompile>
       <WarningLevel>Level3</WarningLevel>
       <Optimization>Disabled</Optimization>
-      <AdditionalIncludeDirectories>..\libs\glew-1.11.0\include;..\libs\glfw-3.0.4\include;..\libs\glm-0.9.5.4\include;..\include\;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
+      <AdditionalIncludeDirectories>..\libs\glew-1.11.0\include;..\libs\glfw-3.0.4\include;..\libs\glm-0.9.5.4\include;..\libs\freetype-2.5.5\include;..\include\;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <DisableSpecificWarnings>4290;4996;4018;%(DisableSpecificWarnings)</DisableSpecificWarnings>
+      <ObjectFileName>$(IntDir)\%(RelativeDir)\</ObjectFileName>
     </ClCompile>
     <Link>
       <GenerateDebugInformation>true</GenerateDebugInformation>

src/Utils/String.cpp
@@ -10,7 +10,7 @@ namespace Util {
         std::string token;
 
         while(std::getline(stream, token, delimiter)) {
-            if(!token.empty() || skipEmpty)
+            if(!token.empty() || !skipEmpty)
                 input.push_back(token);
         }
 