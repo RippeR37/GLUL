dabb22ffdb87936b96f406a483fb2542a2258630
RippeR <ripper37@gmail.com>
2015-02-23 20:44:16 +0100

    [#22] Additional implementation for PixelBuffer wrapper.

--- include/Utils/GL+/PixelBuffer.h
+++ include/Utils/GL+/PixelBuffer.h
@@ -2,19 +2,81 @@
 #define UTILS_GL_PIXEL_BUFFER_H_INCLUDED
 
 #include <Utils/GL+/Buffer.h>
+#include <Utils/GL+/Texture.h>
 
 namespace GL {
 
     class PixelBuffer : public GL::Buffer {
         public:
-            enum class Packing {
+            enum class OperationType {
                 Pack,
                 Unpack
             };
 
+            enum class DataFormat {
+                Red          = GL_RED,
+                Green        = GL_GREEN,
+                Blue         = GL_BLUE,
+                RG           = GL_RG,
+                RGB          = GL_RGB,
+                BGR          = GL_BGR,
+                RGBA         = GL_RGBA,
+                BGRA         = GL_BGRA,
+                RedInt       = GL_RED_INTEGER,
+                GreenInt     = GL_GREEN_INTEGER,
+                BlueInt      = GL_BLUE_INTEGER,
+                RGInt        = GL_RG_INTEGER,
+                RGBInt       = GL_RGB_INTEGER,
+                BGRInt       = GL_BGR_INTEGER,
+                RGBAInt      = GL_RGBA_INTEGER,
+                BGRAInt      = GL_BGRA_INTEGER,
+                Depth        = GL_DEPTH_COMPONENT,
+                Stencil      = GL_STENCIL_INDEX,
+                DepthStencil = GL_DEPTH_STENCIL,
+            };
+
+            enum class DataType {
+                Byte = GL_BYTE,
+                uByte = GL_UNSIGNED_BYTE,
+                Short = GL_SHORT,
+                uShort = GL_UNSIGNED_SHORT,
+                Int = GL_INT,
+                uInt = GL_UNSIGNED_INT,
+                Float = GL_FLOAT,
+                uByte332 = GL_UNSIGNED_BYTE_3_3_2,
+                uByte233R = GL_UNSIGNED_BYTE_2_3_3_REV,
+                uShort565 = GL_UNSIGNED_SHORT_5_6_5,
+                uShort565R = GL_UNSIGNED_SHORT_5_6_5_REV,
+                uShort4444 = GL_UNSIGNED_SHORT_4_4_4_4,
+                uShort4444R = GL_UNSIGNED_SHORT_4_4_4_4_REV,
+                uShort5551 = GL_UNSIGNED_SHORT_5_5_5_1,
+                uShort1555R = GL_UNSIGNED_SHORT_1_5_5_5_REV,
+                uInt8888 = GL_UNSIGNED_INT_8_8_8_8,
+                uInt8888R = GL_UNSIGNED_INT_8_8_8_8_REV,
+                uInt1010102 = GL_UNSIGNED_INT_10_10_10_2,
+                uInt2101010R = GL_UNSIGNED_INT_2_10_10_10_REV,
+            };
+
+            enum class TextureTarget {
+                Tex1D = GL_TEXTURE_1D,
+                Tex2D = GL_TEXTURE_2D,
+                Tex3D = GL_TEXTURE_3D,
+                Tex1DArray = GL_TEXTURE_1D_ARRAY,
+                Tex2DArray = GL_TEXTURE_2D_ARRAY,
+                TexRectangle = GL_TEXTURE_RECTANGLE,
+                TexCubeXPos = GL_TEXTURE_CUBE_MAP_POSITIVE_X,
+                TexCubeXNeg = GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
+                TexCubeYPos = GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
+                TexCubeYNeg = GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
+                TexCubeZPos = GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
+                TexCubeZNeg = GL_TEXTURE_CUBE_MAP_NEGATIVE_Z,
+            };
+
+            typedef Texture::InternalFormat InternalFormat;
+
         public:
             PixelBuffer();
-            PixelBuffer(Packing packing, Usage usage = Usage::StaticCopy);
+            PixelBuffer(OperationType operation, Usage usage = Usage::StaticCopy);
             PixelBuffer(PixelBuffer&& pbo);
             ~PixelBuffer();
 
@@ -24,38 +86,31 @@ namespace GL {
             PixelBuffer& operator=(PixelBuffer&& pbo);
 
             void bind() const;
-            void bind(Packing packing);
+            void bind(OperationType operation);
 
-            void readPixels(GLint x, GLint y, GLint width, GLint height, GLenum format, GLvoid* data);
-            void getTexImage(GLenum target, GLint level, GLenum format, GLenum type, GLvoid* image);
-            void getTexCompressedImage(GLenum target, GLint level, GLvoid* image);
 
-            void setTexImage1D(GLenum target, GLint level, GLint internalFormat, GLsizei width, GLint border, GLenum format, GLenum type, const GLvoid* data);
-            void setTexImage2D(GLenum target, GLint level, GLint internalFormat, GLsizei width, GLsizei height, GLint border, GLenum format, GLenum type, const GLvoid* data);
-            void setTexImage3D(GLenum target, GLint level, GLint internalFormat, GLsizei width, GLsizei height, GLsizei depth, GLint border, GLenum format, GLenum type, const GLvoid* data);
+            void getPixels(GLint x, GLint y, GLint width, GLint height, DataFormat format, DataType type, GLvoid* data);
+            void getTexImage(TextureTarget target, GLint level, DataFormat format, DataType type, GLvoid* image);
+            void getCompressedTexImage(TextureTarget target, GLint level, GLvoid* image);
 
-            /*
-            void setTexSubImage1D();
-            void setTexSubImage2D();
-            void setTexSubImage3D();
+            void setTexImage1D(GLint level, InternalFormat internalFormat, 
+                GLsizei width, GLint border, DataFormat format, DataType type, const GLvoid* data);
 
-            void setTexCompressedImage1D();
-            void setTexCompressedImage2D();
-            void setTexCompressedImage3D();
+            void setTexImage2D(GLint level, InternalFormat internalFormat, 
+                GLsizei width, GLsizei height, GLint border, DataFormat format, DataType type, const GLvoid* data);
 
-            void setTexCompressedSubImage1D();
-            void setTexCompressedSubImage2D();
-            void setTexCompressedSubImage2D();
-            */
+            void setTexImage3D(GLint level, InternalFormat internalFormat,
+                GLsizei width, GLsizei height, GLsizei depth, GLint border, DataFormat format, DataType type, const GLvoid* data);
 
-            void setTarget(Packing packing);
-            void setPacking(Packing packing);
+
+            void setTarget(OperationType operation);
+            void setPacking(OperationType operation);
 
             Target getTarget() const;
-            Packing getPacking() const;
+            OperationType getOperation() const;
 
         private:
-            Packing _packing;
+            OperationType _operation;
     };
 
 }
--- src/Utils/GL+/PixelBuffer.cpp
+++ src/Utils/GL+/PixelBuffer.cpp
@@ -3,15 +3,15 @@
 namespace GL {
 
     PixelBuffer::PixelBuffer() : Buffer(Target::PixelPack) {
-        setPacking(Packing::Pack);
+        setPacking(OperationType::Pack);
     }
 
-    PixelBuffer::PixelBuffer(Packing packing, Usage usage) : Buffer(Target::PixelPack, usage) {
-        setPacking(Packing::Pack);
+    PixelBuffer::PixelBuffer(OperationType operation, Usage usage) : Buffer(Target::PixelPack, usage) {
+        setPacking(operation);
     }
 
     PixelBuffer::PixelBuffer(PixelBuffer&& pbo) : Buffer(std::move(pbo)) {
-        std::swap(_packing, pbo._packing);
+        std::swap(_operation, pbo._operation);
     }
 
     PixelBuffer::~PixelBuffer() {
@@ -21,8 +21,8 @@ namespace GL {
     PixelBuffer& PixelBuffer::operator=(PixelBuffer&& pbo) {
         std::swap(_usage, pbo._usage);
         std::swap(_target, pbo._target);
-        std::swap(_packing, pbo._packing);
         std::swap(_bufferID, pbo._bufferID);
+        std::swap(_operation, pbo._operation);
 
         return *this;
     }
@@ -31,33 +31,59 @@ namespace GL {
         Buffer::bind();
     }
 
-    void PixelBuffer::bind(Packing packing) {
-        setPacking(packing);
+    void PixelBuffer::bind(OperationType operation) {
+        setPacking(operation);
         bind();
     }
 
-    void PixelBuffer::setTarget(Packing packing) {
-        _packing = packing;
 
-        if(packing == Packing::Pack)
+    void PixelBuffer::getPixels(GLint x, GLint y, GLint width, GLint height, DataFormat format, DataType type, GLvoid* data) {
+        glReadPixels(x, y, width, height, static_cast<GLenum>(format), static_cast<GLenum>(type), data);
+    }
+
+    void PixelBuffer::getTexImage(TextureTarget target, GLint level, DataFormat format, DataType type, GLvoid* image) {
+        glGetTexImage(static_cast<GLenum>(target), level, static_cast<GLenum>(format), static_cast<GLenum>(type), image);
+    }
+
+    void PixelBuffer::getCompressedTexImage(TextureTarget target, GLint level, GLvoid* image) {
+        glGetCompressedTexImage(static_cast<GLenum>(target), level, image);
+    }
+
+    void PixelBuffer::setTexImage1D(GLint level, InternalFormat internalFormat, GLsizei width, GLint border, DataFormat format, DataType type, const GLvoid* data) {
+        glTexImage1D(GL_TEXTURE_1D, level, static_cast<GLenum>(internalFormat), width, border, static_cast<GLenum>(format), static_cast<GLenum>(type), data);
+    }
+
+    void PixelBuffer::setTexImage2D(GLint level, InternalFormat internalFormat, GLsizei width, GLsizei height, GLint border, DataFormat format, DataType type, const GLvoid* data) {
+        glTexImage2D(GL_TEXTURE_1D, level, static_cast<GLenum>(internalFormat), width, height, border, static_cast<GLenum>(format), static_cast<GLenum>(type), data);
+    }
+
+    void PixelBuffer::setTexImage3D(GLint level, InternalFormat internalFormat, GLsizei width, GLsizei height, GLsizei depth, GLint border, DataFormat format, DataType type, const GLvoid* data) {
+        glTexImage3D(GL_TEXTURE_1D, level, static_cast<GLenum>(internalFormat), width, height, depth, border, static_cast<GLenum>(format), static_cast<GLenum>(type), data);
+    }
+
+
+    void PixelBuffer::setTarget(OperationType operation) {
+        _operation = operation;
+
+        if(operation == OperationType::Pack)
             Buffer::setTarget(Target::PixelPack);
         else
             Buffer::setTarget(Target::PixelUnpack);
     }
 
-    void PixelBuffer::setPacking(Packing packing) {
-        setTarget(packing);
+    void PixelBuffer::setPacking(OperationType operation) {
+        setTarget(operation);
     }
 
     Buffer::Target PixelBuffer::getTarget() const {
-        if(_packing == Packing::Pack)
+        if(_operation == OperationType::Pack)
             return Target::PixelPack;
         else
             return Target::PixelUnpack;
     }
 
-    PixelBuffer::Packing PixelBuffer::getPacking() const {
-        return _packing;
+    PixelBuffer::OperationType PixelBuffer::getOperation() const {
+        return _operation;
     }
 
 }
