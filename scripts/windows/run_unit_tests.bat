798e576f8a17fb81ea4b9293c0ba8df60b7f4b7d
RippeR37 <ripper37@gmail.com>
2016-04-03 16:34:32 +0200

    [#62] Fixed bugs in TravisCI build script and windows dependency installation script.

--- scripts/windows/run_unit_tests.bat
+++ scripts/windows/run_unit_tests.bat
@@ -7,5 +7,4 @@ echo "### RUNNING UNIT TESTS ###"
 echo "##########################"
 
 cd ..\..\tests\bin\
-move "Unit tests.exe" "unit_tests.exe" # required to have no space in name
-unit_tests.exe
+"Unit tests.exe"
